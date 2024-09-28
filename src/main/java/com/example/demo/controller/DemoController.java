521ac96126677f5a428d008f0cd0318de70419cb
hsun <i12385251@gmail.com>
2024-09-28 16:37:20 +0800

    first commit

---
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -0,0 +1,18 @@
+package com.example.demo.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class DemoController {
+
+
+    @GetMapping("/hello")
+    public String hello(Model model){
+        model.addAttribute("message", "Hello World!0928");
+        return "hello";
+    }
+}
